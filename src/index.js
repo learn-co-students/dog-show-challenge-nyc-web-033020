78c0aadb507f47889b373698e484857f6976818b
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-05-20 11:31:00 -0400

    sthird commit

--- src/index.js
+++ src/index.js
@@ -7,61 +7,40 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function renderDogs(allDogs){
         dogsTable = document.querySelector('#table-body')
-
-        // dogForm = document.querySelector('#dog-form')
-
-        // dogForm.addEventListener('submit', e => {
-        //     e.preventDefault()
-        //     fetch(dogsUrl + '/' +`${dog.id}`, {
-        //         method: 'PATCH',
-        //         headers:{
-        //             'Content-Type': 'application/json',
-        //             'Accept': 'application/json'
-        //         },
-        //         body: JSON.stringify({
-        //             "name": dogForm.name.value,
-        //             "breed": dogForm.breed.value,
-        //             "sex": dogForm.sex.value
-        //         })
-        //     })
-        // })
-
-
+        dogForm = document.querySelector('#dog-form')
+        
         allDogs.forEach(dog => {
             newDogRow = document.createElement('tr')
             newDogRow.innerHTML = `
-                <td>${dog.name}</td> 
-                <td>${dog.breed}</td> 
-                <td>${dog.sex}</td> 
-                <td><button id="dog-${dog.id}">Edit Dog</button></td>
+            <td>${dog.name}</td> 
+            <td>${dog.breed}</td> 
+            <td>${dog.sex}</td> 
+            <td><button data-num="${dog.id}">Edit Dog</button></td>
             `
             dogsTable.append(newDogRow)
-            editBtn = document.querySelector(`#dog-${dog.id}`)
-            dogForm = document.querySelector('#dog-form')
+            editBtn = document.querySelector(`[data-num="${dog.id}"]`)
             editBtn.addEventListener('click', e => {
                 dogForm.name.value = dog.name
                 dogForm.breed.value = dog.breed
                 dogForm.sex.value = dog.sex
             })
         })
-            dogForm.addEventListener('submit', e => {
-                e.preventDefault()
-                fetch(dogsUrl + '/' +`${dog.id}`, {
-                    method: 'PATCH',
-                    headers:{
-                        'Content-Type': 'application/json',
-                        'Accept': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        "name": dogForm.name.value,
-                        "breed": dogForm.breed.value,
-                        "sex": dogForm.sex.value
-                    })
+        dogForm.addEventListener('submit', e => {
+            e.preventDefault()
+            fetch(dogsUrl + '/' + id, {
+                method: 'PATCH',
+                headers:{
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    "name": dogForm.name.value,
+                    "breed": dogForm.breed.value,
+                    "sex": dogForm.sex.value
                 })
             })
-
+        })
     }
-
 })
 
 
