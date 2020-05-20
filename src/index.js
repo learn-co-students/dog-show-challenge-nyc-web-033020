7a8a76a491477a525dd45463c2d0d65d83880181
toastcorners <wise.melissa19@gmail.com>
2020-05-20 10:27:45 -0400

    first commit, only added fetch and pseudocode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+const dogsURL = "http://localhost:3000/dogs"
+const dogPatchURL = "http://localhost:3000/dogs/:id"
+
+
+
+//on page load, render dogs already in table
+
+//dog info should be in a row
+
+//make dog info editable w/ form
+
+//send patch request with dog form
+
+//after patch, table shows edited dog info
+    //-> send get to dogsURL after patch to reflect newest info
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
+fetch(dogsURL)
+.then(resp => resp.json())
+.then(dogs => // insert callback here to populate dog table;
+
+const populateDogTable = () 
+
+
+
 
 })
