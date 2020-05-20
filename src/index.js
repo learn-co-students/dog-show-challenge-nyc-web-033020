101cf78733b9d8e03a69a16d5e4df5b561d66b76
github username <github email address>
2020-05-20 10:25:48 -0400

    get fetch and innerHTML

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //fetch all dogs and append to table on DOM
+    const url = 'http://localhost:3000/dogs'
 
+    fetch(url)
+        .then(resp => resp.json())
+        .then(dogs => {
+            dogs.forEach(dog => {
+                
+            })
+        })
+
+    const table = document.querySelector('#table-body')
+    table.innerHTML = `
+    <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    `
 })
