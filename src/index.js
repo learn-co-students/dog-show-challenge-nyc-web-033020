ad4c5a1ba391ce13a902b829af4325ff7f810ac4
github username <github email address>
2020-05-20 10:50:33 -0400

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dogsUrl = " http://localhost:3000/dogs"
+    const dogsUrl = "http://localhost:3000/dogs"
+    const tableBody = document.getElementById("table-body") 
+    const dogForm = document.getElementById('dog-form') 
+
+    function getDogs() {
+        fetch(dogsUrl) 
+            .then(res => res.json()) 
+            .then(dogs => dogs.forEach(getDog)) 
+    } 
+
+    function getDog(dog) {
+        console.log(dog) 
+        tableBody.innerHTML += `<tr data_id=${dog.id}>
+            <td>${dog.name}</td>
+            <td>${dog.breed}</td>
+            <td>${dog.sex}</td>
+            <td><button id="edit-btn" data-id=${dog.id}>Edit</button></td>
+            </tr>`
+    } 
+    
+    function editDog(id) {
+        fetch(`${dogsUrl}/${id}`) 
+            .then(res => res.json()) 
+            .then(dog => {
+                dogForm.name.value = dog.name,
+                dogForm.breed.value = dog.breed,
+                dogForm.sex.value = dog.sex,
+                dogForm.dataset.id = dog.id  
+            })
+    }
+
+    function editedDog(e) {
+        let dog = {
+            
+        }
+    }
 })
