fba0517744deecce3c9f6f5a087b416b17e52731
toastcorners <wise.melissa19@gmail.com>
2020-05-20 11:33:48 -0400

    second commit, table is not working the way I'd expect

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const dogsURL = "http://localhost:3000/dogs"
 const dogPatchURL = "http://localhost:3000/dogs/:id"
 
 
-
-//on page load, render dogs already in table
+//on page load, render existing dogs set inside of table format
+        //-> fetch dogs
+        //-> send dogs to callback that populates table
 
 //dog info should be in a row
+        //-> assumed setup in readme
 
 //make dog info editable w/ form
 
@@ -18,13 +20,39 @@ const dogPatchURL = "http://localhost:3000/dogs/:id"
 
 
 document.addEventListener('DOMContentLoaded', () => {
-fetch(dogsURL)
-.then(resp => resp.json())
-.then(dogs => // insert callback here to populate dog table;
+    //fetch dog info when page loads
+    fetch(dogsURL)
+    .then(resp => resp.json())
+    .then(json => populateDogTable(json));
 
-const populateDogTable = () 
+//create dog table for later use
+const dogTable = document.createElement('table')
+
+
+//populate dog table
+const populateDogTable = dogs => {
+dogs.forEach(dog => {
+    console.log(dogTable)
+    dogTable.innerHTML =  `
+        <tr>
+        <td>${dog.name}</td> 
+        <td>${dog.breed}</td> 
+        <td>${dog.sex}</td> 
+        <td><button>Edit</button></td>
+        </tr>
+            `
+    })
+}
 
 
 
+
+//dogTableBody.appendChild()
 
 })
+
+
+
+
+
+
