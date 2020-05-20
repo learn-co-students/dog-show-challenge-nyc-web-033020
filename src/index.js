bbc8b5b6e85855261ffecfe88ec28f6dd9e3c420
github username <github email address>
2020-05-20 11:38:33 -0400

    4th commit

--- src/index.js
+++ src/index.js
@@ -1,48 +1,60 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const dogsUrl = "http://localhost:3000/dogs"
-    const tableBody = document.getElementById("table-body") 
-    const dogForm = document.getElementById('dog-form') 
 
-    function getDogs() {
-        fetch(dogsUrl) 
-            .then(res => res.json()) 
-            .then(dogs => dogs.forEach(getDog)) 
-    } 
-
-    function getDog(dog) {
-        console.log(dog) 
-        tableBody.innerHTML += `<tr data_id=${dog.id}>
-            <td>${dog.name}</td>
-            <td>${dog.breed}</td>
-            <td>${dog.sex}</td>
-            <td><button id="edit-btn" data-id=${dog.id}>Edit</button></td>
-            </tr>`
-    } 
+    const dogsUrl = `http://localhost:3000/dogs`
+    const tableBody = document.getElementById('table-body')
+    document.addEventListener("click", handleEvents)
+    const dogForm = document.getElementById('dog-form')
     
-    function editDog(id) {
-        fetch(`${dogsUrl}/${id}`) 
-            .then(res => res.json()) 
-            .then(dog => {
-                dogForm.name.value = dog.name,
-                dogForm.breed.value = dog.breed,
-                dogForm.sex.value = dog.sex,
-                dogForm.dataset.id = dog.id  
-            })
+    fetch(dogsUrl)
+    .then(res => res.json())
+    .then(dogs => dogs.forEach(getDog))
+    
+    function getDog(dog){
+      console.log(dog)
+      tableBody.innerHTML += `<tr data-id=${dog.id}><td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button id="edit-btn" data-id=${dog.id}>Edit</button></td></tr>`
     }
-
-    function editedDog(e) {
-        let dog = {
-            name: e.target.parentElement.name.value, 
-            breed: e.target.parentElement.breed.value,
-            sex: e.target.parentElement.sex.value
-        } 
-
-        fetch(dogsUrl + id, {
-            method: "PATCH", 
-            headers: {
-                "Content-Type": "application/json", 
-                Accept: "application/json"
-            }
-        })
+    
+    function handleEvents(e){
+      e.preventDefault()
+      if(e.target.id === "edit-btn"){
+        editDog(e.target.dataset.id)
+      } else if(e.target.parentElement.id === 'dog-form'){
+        editedDog(e)
+      }
     }
+    
+    function editDog(id){
+      fetch(`${dogsUrl}/${id}`)
+      .then(res => res.json())
+      .then(dog => {
+        dogForm.name.value = dog.name,
+        dogForm.sex.value = dog.sex,
+        dogForm.breed.value = dog.breed,
+        dogForm.dataset.id = dog.id
+      })
+    
+    }
+    
+    function editedDog(e){
+      let dog = {
+        name: e.target.parentElement.name.value,
+        sex: e.target.parentElement.sex.value,
+        breed: e.target.parentElement.breed.value
+  }
+
+  fetch(`${dogsUrl}/${e.target.parentElement.dataset.id}`, {
+    method: 'PATCH',
+    headers: {
+      "content-type": 'application/json',
+      accepts: 'application/json'
+    },
+    body: JSON.stringify(dog)
+  }).then(res => res.json())
+  .then(dog => {
+    let foundDog = document.querySelector(`tr[data-id="${dog.id}"]`)
+    foundDog.children[0].innerText = dog.name
+    foundDog.children[1].innerText = dog.breed
+    foundDog.children[2].innerText = dog.sex
+  })
+}
 })
