6101e9ab247ce2ad6c734edc57b4668ebdcdce2b
github username <github email address>
2020-05-20 10:46:15 -0400

    fsecond commit

--- src/index.js
+++ src/index.js
@@ -2,23 +2,46 @@ document.addEventListener('DOMContentLoaded', () => {
 //fetch url and render a list of dogs 
 //add dogs into table 
 const url = 'http://localhost:3000/dogs'
+const table = document.getElementById('table-body')
+const paddingClass = document.getElementsByClassName('padding-center')
 
 fetch(url)
 .then(res => res.json())
 .then(dogs => renderDogs(dogs))
 
 const renderDogs = dog => {
-    console.log(dog)
-
     const dogInfo = dog.forEach(element  => {
-        `<tr>
-        <td>Dog Name: ${element.name} </td>
-        <td>Dog Breed: ${element.breed} </td>
-        <td>Dog Sex: ${element.sex} </td>
-        <td><button>Edit</button></td></tr>`
+    `<tr>
+        <td>${element.name} </td>
+        <td>${element.breed} </td>
+        <td>${element.sex} </td>
+        <td><button>Edit</button></td>
+    </tr>`
     })
 }
 
 
 
+
+// function createTable() {
+//     const tbl = document.createElement('table')
+//     const tBody = document.createElement('tbody')
+//     for (let i = 0; i < 2; i++) {
+//         const tRow = document.createElement('tr');
+    
+//         for (let j = 0; j < 2; j++) {
+//           let cell = document.createElement('td');
+//           let cellText = document.createTextNode("cell in row "+i+", column "+j);
+//           cell.appendChild(cellText);
+//           row.appendChild(cell);
+//         }
+//         tblBody.appendChild(row);
+//       }
+//       tbl.appendChild(tBody);
+//       table.appendChild(tbl);
+//       tbl.setAttribute("border", "2");
+// }
+
 })
+
+//
