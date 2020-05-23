59aaf3a5ef146f094cbf8e5aff74f129e87306ae
github username <github email address>
2020-05-23 15:24:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,86 @@
 document.addEventListener('DOMContentLoaded', () => {
-//fetch url and render a list of dogs 
-//add dogs into table 
-const url = 'http://localhost:3000/dogs'
-const table = document.getElementById('table-body')
-const paddingClass = document.getElementsByClassName('padding-center')
 
-fetch(url)
-.then(res => res.json())
-.then(dogs => renderDogs(dogs))
+//√ GET request to /dogs
 
-const renderDogs = dog => {
-    const dogInfo = dog.forEach(element  => {
-    `<tr>
-        <td>${element.name} </td>
-        <td>${element.breed} </td>
-        <td>${element.sex} </td>
-        <td><button>Edit</button></td>
-    </tr>`
+//√ render each dog as a table row 
+
+//√click listener for edit buttons - delegation 
+//√pull dog's data from the table -> 
+//√enter it into form 
+
+//√submit listener
+//√get data out of form
+//√PATCH request to /dogs/:id - √data binding - √update dog info 
+
+//re-render dogs colelction (i.e. redo get request, etc)
+
+  const url = 'http://localhost:3000/dogs'
+  const tableBody = document.querySelector('#table-body')
+
+    const getDogs = () => {
+        fetch(url)
+        .then(res => res.json())
+        .then(renderDogs)
+    }
+
+    const renderDogs = dogs => {
+        tableBody.innerHTML = ''
+        dogs.forEach(dog => {
+            const row = document.createElement('tr')
+            row.dataset.id = dog.id
+            row.innerHTML = `
+            <td>${dog.name}</td>
+            <td>${dog.breed}</td>
+            <td>${dog.sex}</td>
+            <td> 
+                <button class = 'edit'>Edit</button>
+            </td>
+            `    
+            tableBody.append(row)
+         })
+    }
+
+    tableBody.addEventListener('click', event => {
+        if(event.target.className === 'edit') { //event is the 'click', target is what the event listener is on, in this case - edit
+            const row = event.target.parentNode.parentNode // first parentNode of event.target = cells (<td></td>), second = row (<tr></tr>)
+            const cells = row.getElementsByTagName('td')
+
+            const name = cells[0].textContent 
+            const breed = cells[1].textContent
+            const sex = cells[2].textContent
+
+            const form = document.querySelector('#dog-form')
+
+            form.name.value = name 
+            form.breed.value = breed
+            form.sex.value = sex 
+            
+            form.dataset.id = row.dataset.id 
+        }
     })
-}
 
+    document.addEventListener('submit', event => {
+        event.preventDefault()
+        const form = event.target 
+        const id = form.dataset.id 
+        const name = form.name.value 
+        const breed = form.breed.value 
+        const sex = form.sex.value 
 
+        fetch(`${url}/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'accept': 'application/json',
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({name, breed, sex})
+        })
+        .then(res => res.json())
+        .then(getDogs)
+    })
 
-
-// function createTable() {
-//     const tbl = document.createElement('table')
-//     const tBody = document.createElement('tbody')
-//     for (let i = 0; i < 2; i++) {
-//         const tRow = document.createElement('tr');
-    
-//         for (let j = 0; j < 2; j++) {
-//           let cell = document.createElement('td');
-//           let cellText = document.createTextNode("cell in row "+i+", column "+j);
-//           cell.appendChild(cellText);
-//           row.appendChild(cell);
-//         }
-//         tblBody.appendChild(row);
-//       }
-//       tbl.appendChild(tBody);
-//       table.appendChild(tbl);
-//       tbl.setAttribute("border", "2");
-// }
-
+    getDogs()
+//  
 })
 
-//
+
